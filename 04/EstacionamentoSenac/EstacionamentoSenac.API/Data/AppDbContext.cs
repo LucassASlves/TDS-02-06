ea0e5c2558496c0cd1d63af5c380cafd58a889c5
LUCAS ALVES CANDIDO <lucas.acandido1@senacsp.edu.br>
2025-10-22 20:37:47 -0300

    Primeiro commit

---
+++ 04/EstacionamentoSenac/EstacionamentoSenac.API/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using EstacionamentoSenac.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EstacionamentoSenac.API.Data
+{
+    public class AppDbContext : DbContext
+    {
+
+        public DbSet<Veiculo> Veiculo { get; set; }
+    }
+}
